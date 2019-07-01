2d170e989f43ab64bc7fa7c7c4065f033bc2e5fa
Phyllis Kan <pyk9q7@gmail.com>
2019-07-01 19:35:11 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,7 +3,8 @@ def hello_t
 end
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
-if name.start_with?("T")
-  puts "Hi, #{name}"
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
 end
 # call your method here!
